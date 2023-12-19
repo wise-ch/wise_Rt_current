e338726195a9c0861ae84532b2f9b394c44f2a2f
Adrian Lison <adrian.lison@bsse.ethz.ch>
2023-12-19 12:34:07 +0100

    add scripts for running dashboard

---
+++ run_app.R
@@ -0,0 +1,14 @@
+if (!require("shiny")){
+  install.packages("shiny")
+}
+if (!require("dplyr")){
+  install.packages("dplyr")
+}
+if (!require("ggplot2")){
+  install.packages("ggplot2")
+}
+
+shiny::runGitHub(
+  "wise-ch/wise_Rt_dashboard_internal", ref = "refs/heads/main", subdir = "app",
+  launch.browser = TRUE
+  )
